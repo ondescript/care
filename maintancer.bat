3e080c0fb4b2d6250e1c92442c6bd4c2b7fd7c6d
ondescript <non-descript@hotmail.com>
2014-11-10 18:09:48 +0200

    Create maintancer.bat
    microsoft windows xp os for on msdos embedded tools perform maintenance as batch file

---
+++ maintancer.bat
@@ -0,0 +1,24 @@
+echo off
+title ntfs sistemler icin bakim onarim ve optmizasyoncu
+color 06
+cls
+prompt $l
+echo y |chkdsk /f/r/l d:
+echo y |chkdsk /f/r/l c:
+echo y |chkdsk /f/r/l e:
+echo y |chkdsk /f/r/l f:
+echo y |chkdsk /f/r/l g:
+echo y |chkdsk /f/r/l h:
+echo y |chkdsk /f/r/l j:
+defrag /f/v d:
+defrag /f/v c:
+defrag /f/v e:
+defrag /f/v f:
+defrag /f/v g:
+defrag /f/v h:
+shutdown -s -t 160
+echo =====================================================================
+echo =========bir tusa basmaz isen bilgisayar kendini kapatacak==========
+echo =====================================================================
+pause
+shutdown -a
